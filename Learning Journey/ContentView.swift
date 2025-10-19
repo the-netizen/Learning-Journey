81a3512773e40b80808c37f606684dab76af9f24
the-netizen <khanaima020815@gmail.com>
2025-10-19 15:00:29 +0300

    Initial Commit

---
+++ Learning Journey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Learning Journey
+//
+//  Created by Naima Khan on 19/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Learning Journey/Learning_JourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_JourneyApp.swift
+//  Learning Journey
+//
+//  Created by Naima Khan on 19/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Learning_JourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
