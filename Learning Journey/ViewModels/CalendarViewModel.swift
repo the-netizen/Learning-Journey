ec92f621fa2f5a7f72cd42f9c51d703d3c3efaf5
the-netizen <khanaima020815@gmail.com>
2025-10-29 15:38:58 +0300

    base calendar display without streak

--- Learning Journey/ViewModels/CalendarViewModel.swift
+++ Learning Journey/ViewModels/CalendarViewModel.swift
@@ -8,6 +8,19 @@
 import Foundation
 
 class CalendarViewModel : ObservableObject{
+    @Published var currentDate = Date()
+//    @Published var selectedDate = Date()
+    private let calendar = Calendar.current
     
     init(){}
+       
+    func getMonthRange(month: Int) -> ClosedRange<Date> {
+            let calendar = Calendar.current
+            let monthDate = calendar.date(byAdding: .month, value: month, to: Date()) ?? Date()
+            let start = calendar.date(from: calendar.dateComponents([.year, .month], from: monthDate))!
+            let end = calendar.date(byAdding: DateComponents(month: 1, day: -1), to: start)!
+            return start...end
+    }
+    
 }
+
